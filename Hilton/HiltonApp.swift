0d3ef604cae189bc723ddd48feb6a73dc269f205
nurs.syhn <nursultan.seyhan@gmail.com>
2023-07-04 11:12:27 +0300

    5 menülü modül ve harita sayfası yapımı

--- Hilton/HiltonApp.swift
+++ Hilton/HiltonApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct HiltonApp: App {
     var body: some Scene {
         WindowGroup {
+            
             ContentView()
         }
     }
---
+++ Hilton/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  Hilton
+//
+//  Created by Nur Sultan Şeyhanlıoğlu on 4.07.2023.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
